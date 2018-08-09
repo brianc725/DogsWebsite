f70663da7ec022b6eabeb9c5f9c2210f98c70be0
brianc725 <brianc725@gmail.com>
2018-08-08 22:38:59 -0700

    parse for breed name and display with random image

--- src/app/randomdog/randomdog.component.ts
+++ src/app/randomdog/randomdog.component.ts
@@ -2,25 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
 
-interface Random {
-  status: string;
-  message: string;
-}
-
 @Component({
   selector: 'app-randomdog',
   templateUrl: './randomdog.component.html',
   styleUrls: ['./randomdog.component.scss']
 })
+
 export class RandomdogComponent implements OnInit {
 
   Image: Object;
+  Breed: string;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
     this.data.getRandom().subscribe(
-      data => this.Image = data //gets the Image object from the Dog API
+      (data) => {
+            this.Image = data; //gets the Image object from the Dog API
+
+            var parser = document.createElement('a'); //used to grab the path name where the breed is
+            parser.href = data['message'];
+            var pathname = parser.pathname; 
+            var split = pathname.split('/');
+            this.Breed = split[2]; //the breed name is in the second part of the path
+          }
     );
   }
 
