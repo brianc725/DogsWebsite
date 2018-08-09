01fd90200e29637ccca5d4ac9ce634f3e59416a0
brianc725 <brianc725@gmail.com>
2018-08-08 20:09:36 -0700

    update styling and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ import { BreedslistComponent } from './breedslist/breedslist.component'
 const routes: Routes = [
   {
     path: '',
+    redirectTo: "random",
+    pathMatch: "full"
+  },
+  {
+    path: "random",
     component: RandomdogComponent
   },
   {
